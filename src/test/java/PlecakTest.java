9c7e60da83ff0a8f4c81c29abde2597f5da20c15
TymoteuszKowalski <tymek@gft.pl>
2025-11-28 10:52:27 +0100

    Update PlecakTest.java

--- src/test/java/PlecakTest.java
+++ src/test/java/PlecakTest.java
@@ -3,56 +3,28 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PlecakTest {
-    @Test
-    public void zwrociZawartoscKomory1a(){
-        //given
-        String z = "vJrwpWtwJgWrhcsFMMfFFhFp";
-        Plecak plecak = new Plecak(z);
-
-        //when
-        String k1 = plecak.zawartoscKomory1();
-
-        //then
-        Assertions.assertEquals("vJrwpWtwJgWr", k1);
-    }
-
-    @Test
-    public void zwrociZawartoscKomory1b(){
-        //given
-        String z = "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL";
-        Plecak plecak = new Plecak(z);
-
-        //when
-        String k2 = plecak.zawartoscKomory2();
-
-        //then
-        Assertions.assertEquals("jqHRNqRjqzjGDLGL", k2);
-    }
-
-    @Test
-    public void zwrociZleSpakowanyElement1(){
-        //given
-        String z = "vJrwpWtwJgWrhcsFMMfFFhFp";
-        Plecak plecak = new Plecak(z);
-
-        //when
-        char element = plecak.zleSpakowanyElement1();
-
-        //then
-        Assertions.assertEquals('p', element);
-    }
-
-    @Test
-    public void zwrociPriorytetElementu1(){
-        //given
-        char znak = 'c';
-        String z = "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL";
-        Plecak plecak = new Plecak(z);
-
-        //when
-        int priorytet = plecak.priorytetElementu(znak);
-
-        //then
-        Assertions.assertEquals(3,priorytet);
-    }
+   @Test
+   public void zwrociZawartoscKomory1() {
+       Plecak p = new Plecak("vJrwpWtwJgWrhcsFMMfFFhFp");
+       Assertions.assertEquals("vJrwpWtwJgWr", p.zawartoscKomory1());
+   }
+    
+   @Test
+   public void zwrociZawartoscKomory2() {
+       Plecak p = new Plecak("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL");
+       Assertions.assertEquals("rsFMfFZSrLrFZsSL", p.zawartoscKomory2());
+   }
+    
+   @Test
+   public void zwrociZleSpakowanyElement() {
+       Plecak p = new Plecak("vJrwpWtwJgWrhcsFMMfFFhFp");
+       Assertions.assertEquals('p', p.zleSpakowanyElement1());
+   }
+    
+   @Test
+   public void priorytetZnaku() {
+       Plecak p = new Plecak("x");
+       Assertions.assertEquals(3, p.priorytetElementu('c'));
+       Assertions.assertEquals(27, p.priorytetElementu('A'));
+   }
 }
