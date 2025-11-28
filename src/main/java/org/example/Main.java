4346109c0da8cde20f7bb49ff07f543f3fc2cad5
TymoteuszKowalski <tymek@gft.pl>
2025-11-28 10:51:23 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+   public static void main(String[] args) throws Exception {
+       List<String> linie = Files.readAllLines(Path.of("input.txt"));
+       RucksackSolver solver = new RucksackSolver();
+       int wynik = solver.sumPriority(linie);
+       System.out.println("Suma priorytetów = " + wynik);
+   }
 }
