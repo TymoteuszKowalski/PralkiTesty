11494a0b41f1e9628c78f2bc9776b2b70720cd19
TymoteuszKowalski <tymek@gft.pl>
2025-11-28 10:43:57 +0100

    Update Plecak.java

--- src/main/java/org/example/Plecak.java
+++ src/main/java/org/example/Plecak.java
@@ -1,37 +1,44 @@
 package org.example;
 
 public class Plecak {
+
     private String zawartosc;
 
     public Plecak(String zawartosc) {
         this.zawartosc = zawartosc;
     }
 
-
     public String zawartoscKomory1() {
-        return zawartosc.substring(0,zawartosc.length()/2);
+        int half = zawartosc.length() / 2;
+        return zawartosc.substring(0, half);
     }
 
     public String zawartoscKomory2() {
-        return zawartosc.substring(0,zawartosc.length()/2);
+        int half = zawartosc.length() / 2;
+        return zawartosc.substring(half);
     }
-
+    
     public char zleSpakowanyElement1() {
         String k1 = zawartoscKomory1();
         String k2 = zawartoscKomory2();
-
-        for (char z1:k1.toCharArray()) {
-            for (char z2:k2.toCharArray()) {
+        for (char z1 : k1.toCharArray()) {
+            for (char z2 : k2.toCharArray()) {
                 if (z1 == z2) return z1;
             }
         }
-
         return ' ';
     }
 
-    public int priorytetElementu(char z){
-        //TODO
+    public int priorytetElementu(char z) {
+        if (z >= 'a' && z <= 'z') {
+            return z - 'a' + 1;
+        }
+        if (z >= 'A' && z <= 'Z') {
+            return z - 'A' + 27;
+        }
+        return 0;
 
-        return 3;
     }
+
 }
+ 
